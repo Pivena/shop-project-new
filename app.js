e447f9cc2c0436b6058e4f8c66c7db2674aba990
Anna <bcworldcares@gmail.com>
2022-05-18 20:09:10 +0300

    add slider

--- app.js
+++ app.js
@@ -14,6 +14,17 @@ for (let i = 0; i < addToCartBtns.length; i++) {
 const modal = document.querySelector(".modal");
 const moreDetailsBtns = document.querySelectorAll(".btn-more-details");
 
+function openModal() {
+    modal.classList.add("show");
+    modal.classList.remove("hide");
+  }
+  
+  function closeModal() {
+    modal.classList.add("hide");
+    modal.classList.remove("show");
+  }
+  
+
 moreDetailsBtns.forEach((btn) => {
     btn.addEventListener("click", function () {
         modal.classList.add("show");
@@ -26,15 +37,117 @@ btnClose.addEventListener("click", function(){
     modal.classList.add("hide");
 });
 
-// like
-const likes = document.querySelectorAll(".like");
-
-for (let like of likes) {
-    like.addEventListener("click", function(){
-    like.classList.toggle("likeChosen");
+modal.addEventListener("click", function(e){
+    if(e.target === modal){
+        closeModal();
+    }
 });
- 
+
+/*function showModalByScroll(){
+    if (window.pageYOffset > document.body.scrollHeight / 2) {
+        openModal();
+        window.removeEventListener("scroll", showModalByScroll);
+    }
+}
+
+window.addEventListener("scroll", showModalByScroll);*/
+
+/* like
+const likeBtns = document.querySelectorAll(".like");
+
+likeBtns.forEach(item) => {
+    item.addEventListener("click", function(){
+        item.classList.toggle("liked")
+    });
+}
+*/
+/*slick*/
+
+$(".slider").slick({
+    dots: true,
+});
+
+/*change product quantity
+const decrementBtns = document.querySelectorAll(".decrement-button");
+const incrementBtns = document.querySelectorAll(".increment-button");
+const quantityInputs = document.querySelectorAll(".product-quantity input");
+
+
+const currentValue = +quantityInput.value;
+toggleButtonState(currentValue)
+
+    function toggleButtonState(count) {
+    decrementBtns.disabled = count <= 1;
+    incrementBtns.disabled = count >= 5;
+  }
+  
+incrementBtns.forEach(incrementBtns, i)
+
+incrementBtns.addEventListener("click", function () {
+    const nextValue = +quantityInput.value + 1;
+    quantityInput.value = nextValue;
+
+    if (nextValue <= 1) {
+        decrementBtns.disabled = true;
+    } else {
+        decrementBtns.disabled - false;
+    }
+});
+decrementBtns.addEventListener("click", function () {
+    const nextValue = +quantityInput.value + 1;
+    quantityInput.value = nextValue;
+
+    if (nextValue <= 1) {
+        decrementBtns.disabled = true;
+    } else {
+        decrementBtns.disabled - false;
+    }
+});
+*/
+
+const decrementBtns = document.querySelectorAll(".decrement-button");
+const incrementBtns = document.querySelectorAll(".increment-button");
+const quantityInputs = document.querySelectorAll(".product-quantity input");
+
+function Counter(incrementBtn, decrementBtn, quantityInput, minCount = 1, maxCount = 5){
+ this.domRefs = {
+     incrementBtn,
+     decrementBtn,
+     quantityInput,
+    };
+
+
+
+
+ this.toggleButtonState = function () {
+    const count = this.domRefs.quantityInput.value;
+    this.domRefs.decrementBtn.disabled = count <= minCount;
+    this.domRefs.incrementBtn.disabled = count >= maxCount;
  };
 
+ this.toggleButtonState();
 
 
+
+this.increment = function () {
+    this.domRefs.quantityInput.value = +this.domRefs.quantityInput.value + 1;
+    this.toggleButtonState();
+  };
+
+  this.decrement = function () {
+    this.domRefs.quantityInput.value = +this.domRefs.quantityInput.value - 1;
+    this.toggleButtonState();
+  };
+
+  this.domRefs.incrementBtn.addEventListener("click", this.increment.bind(this));
+
+  this.domRefs.decrementBtn.addEventListener("click", this.decrement.bind(this));
+}
+
+const counter1 = new Counter(
+    incrementBtns[0],
+    decrementBtns[0],
+    quantityInputs[0]
+);
+
+console.log(counter1);
